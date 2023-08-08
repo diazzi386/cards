f574f0f88f70a166b9d42af4d1c3b0494e27e499
diazzi386 <diazzi386@gmail.com>
2023-08-08 13:17:47 +0000

    varie

--- cards.js
+++ cards.js
@@ -1,21 +1,21 @@
 // Change here deck specs
-var Deck = {
-  suits: ["♠", "♥", "♣", "♦"],
-  numbers: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-  decks: 1,
+const suits = ["♥", "♦", "♣", "♠"];
+const colors = ["crimson", "crimson", "black", "black"]
+const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const decks = 1;
+
+var deck = {
   cards: [],
   shuffle: function () {
     var temp = [];
-    for (var i = 0; i < this.suits * this.numbers * this.decks; i++)
-      temp.push(i % (this.suits * this.numbers));
-    
-    cards = [];
-    for (var i = 0; i < this.suits * this.numbers * this.decks; i++) {
+    for (var i = 0; i < suits.length * numbers.length; i++)
+      temp.push(i % (suits.length * numbers.length));
+    this.cards = [];
+    for (var i = 0; i < suits.length * numbers.length; i++) {
       var n = Math.floor(Math.random() * temp.length);
       this.cards.push(temp[n]);
       temp.splice(n, 1);
     }
-  
     console.log("The deck has been shuffled.");
   }, draw: function () {
     if (this.cards.length > 1)
@@ -26,4 +26,17 @@ var Deck = {
   }
 };
 
-Deck.shuffle();
+var card = {
+  label: function (card) {
+    if (card >= 0 && card < suits.length * numbers.length)
+    return numbers[card % numbers.length] + suits[Math.floor(card / numbers.length)];
+  }, color: function (card) {
+    return colors[Math.floor(card / numbers.length)];
+  }
+}
+
+deck.shuffle();
+
+for (var i in deck.cards) {
+  document.getElementById("cards").innerHTML += "<div class='card' style='color: " + card.color(deck.cards[i]) + "'>" + card.label(deck.cards[i]) + "</div>";
+}
