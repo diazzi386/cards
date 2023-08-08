1a0685d69af672abe23274c05d68cf359d602007
diazzi386 <diazzi386@gmail.com>
2023-08-08 08:43:22 +0000

    Varie

--- cards.js
+++ cards.js
@@ -1,13 +1,26 @@
-var Deck = function (suits, numbers, decks) {
+var Deck = function (suits, numbers, decks = 1) {
   this.suits = suits;
   this.numbers = numbers;
   this.decks = decks;
   this.total = this.suits.length * this.numbers.length * this.decks.length;
   this.cards = [];
-  for (var i = 0; i++; i < this.total)
-    this.cards[i] = i;
+
+  var unshuffled = [];
+  for (var i = 0; i < this.total; i++)
+    unshuffled[i] = i;
+  
+  for (var i = 0; i < this.total; i++) {
+    var n = Math.floor(Math.random() * (i));
+    this.cards.push(unshuffled[n]);
+    this.cards.push(unshuffled[n]);
+    unshuffled[n] = unshuffled.pop();
+    unshuffled.push(this.cards[i]);
+  }
 
   this.draw = function () {
-
   }
+
+  return this;
 }
+
+var deck = new Deck(["", "", "", ""], ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]);
